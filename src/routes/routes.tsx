26a2c7e29ef14d2067dd027e6339ed8fb3edf73e
Mursalin <mursalin2311@gmail.com>
2025-01-12 22:23:31 +0600

    installed  router dom

---
+++ src/routes/routes.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default router;
