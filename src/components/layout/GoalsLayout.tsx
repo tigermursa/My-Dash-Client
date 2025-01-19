c238c330b2a112d51e448dea5c97a9e520909148
Mursalin <mursalin2311@gmail.com>
2025-01-20 03:24:07 +0600

    added todo for day to year nut i will make it more reusable component for less boilerplate

--- src/components/layout/GoalsLayout.tsx
+++ src/components/layout/GoalsLayout.tsx
@@ -1,5 +1,24 @@
-const GoalsLayout = () => {
-  return <div></div>;
+import { ReactNode } from "react";
+import MiniCalendar from "../GoalsAddons/MiniCalendar";
+
+interface MainLayoutProps {
+  children: ReactNode;
+}
+
+const GoalsLayout = ({ children }: MainLayoutProps) => {
+  return (
+    <div className="flex justify-between flex-col sm:flex-row h-full dark:bg-gray-900">
+      {/* Main Content Section */}
+      <div className="w-full sm:w-[78%] h-full">
+        <div className="h-full overflow-y-auto scrollbar-none">{children}</div>
+      </div>
+
+      {/* MiniCalendar Section - hidden on small screens */}
+      <div className="w-full sm:w-[20%] sm:block hidden sm:border-l border-gray-300 dark:border-gray-700 p-4 sm:h-auto h-full">
+        <MiniCalendar />
+      </div>
+    </div>
+  );
 };
 
 export default GoalsLayout;
