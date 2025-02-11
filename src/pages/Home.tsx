b499655fd6b0b17f3512dc6c47aba44f3d1ed93e
Mursalin <mursalin2311@gmail.com>
2025-02-11 22:16:50 +0600

    tested build to see everything working perfectly

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -153,7 +153,7 @@ const Home = () => {
                   `${name} ${(percent * 100).toFixed(0)}%`
                 }
               >
-                {categoryData.map((entry, index) => (
+                {categoryData.map((_entry, index) => (
                   <Cell
                     key={`cell-${index}`}
                     fill={COLORS[index % COLORS.length]}
